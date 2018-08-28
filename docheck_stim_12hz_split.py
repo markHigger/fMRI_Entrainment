9b55b5d561045d2ccc0916da81febf0f6628607b
markHigger <markbhigger@gmail.com>
2018-08-28 13:36:38 -0400

    Cleaned up code
    renamed vars, reorganized and added comments

--- docheck_stim_12hz_split.py
+++ docheck_stim_12hz_split.py
@@ -11,21 +11,56 @@ class MyParser(argparse.ArgumentParser):
         sys.stderr.write('error: %s\n' % message)
         self.print_help()
         sys.exit(2)
+""" Function Description
+to call :
+    'python docheck_stim_12hz_split <args>'
 
+Arguments:
+    Required:
+        -subid - id of subject
+        -seconds - aproxamate length of video in seconds (will round up to required val)
+            -default: 60s
+        -tside - side target should apear on
+            -default: right
+        -tcpsec - Freq of target side checkerboard fading in Hz
+            -either: 0.1, 0.2, 0.5 or 1.0
+            -default: 0.5
+        -tcpsec - Freq of non-target side checkerboard fading in Hz
+            -either: 0.1, 0.2, 0.5 or 1.0
+            -default: 0.5
+"""
 #true if in scanner
 inScanner = False
-        
+
+###############################################################################
+#Parse Inputs
+
 parser = MyParser(prog="docheck_stim")
-parser.add_argument('-subid', dest='subid', help="Subject ID. (Required)", required=True)
-parser.add_argument('-seconds', dest='secs', help="Approximate number of seconds you want the task to last (will be rounded up to generate full target cycle).", default=60)
-parser.add_argument('-tside', dest='side', help = 'What side the target should appear on', default='r',type=str)
+
+parser.add_argument('-subid', dest='subid', 
+                    help="Subject ID. (Required)", required=True)
+
+parser.add_argument('-seconds', dest='secs', 
+                    help="Approximate number of seconds you want the task to last (will be rounded up to generate full target cycle).", 
+                    default=60)
+
+parser.add_argument('-tside', dest='side', 
+                    help = 'What side the target should appear on', 
+                    default='r',type=str)
+
 #how fast the opacity changes/how often the dot appears
-parser.add_argument('-tcpsec', dest='tcps', help='Cycles per second (hz)', choices=[0.1, 0.2, 0.5, 1.0], default=0.5, type=float)
-parser.add_argument('-ntcpsec', dest='ntcps', help='Cycles per second (hz)', choices=[0.1, 0.2, 0.5, 1.0], default=0.5, type=float)
+parser.add_argument('-tcpsec', dest='tcps', 
+                    help='Cycles per second (hz)', 
+                    choices=[0.1, 0.2, 0.5, 1.0], default=0.5, type=float)
+
+parser.add_argument('-ntcpsec', dest='ntcps',
+                    help='Cycles per second (hz)', 
+                    choices=[0.1, 0.2, 0.5, 1.0], default=0.5, type=float)
+
 #how fast the flicker goes
-parser.add_argument('-fcpsec',dest='fcps',help='Cycles per second (hz) of flicker', default=12., type=float)
-parser.add_argument("-type", dest="type", help="Type of stimulus display (boxcar or sine)", 
-	choices=['sine','boxcar'],default='sine')
+parser.add_argument('-fcpsec',dest='fcps',
+                    help='Cycles per second (hz) of flicker', 
+                    default=12., type=float)
 
 #true if should show instructions
 show_inst = False
@@ -35,42 +70,15 @@ inst_text = ['The experiment will begin shortly.',
 
 psychopy.event.clearEvents()
 
+#get arguments
 args = parser.parse_args()
+flicker = float(args.fcps)
 seconds = float(args.secs)
+#mulitply frequency times 2 to get correct frequency
 tcps = args.tcps * 2
 ntcps = args.ntcps * 2
-block_len = (1/tcps) * (1/ntcps) * 10
-flicker = float(args.fcps)
-seconds += (seconds % block_len)
-block_num = int(seconds / block_len)
-tnum_pi = float(tcps) * block_len
-ntnum_pi = float(ntcps) * block_len
-step = int((flicker*block_len) / (tnum_pi / 2))
-if args.type == 'sine':
-	x = np.linspace(0,tnum_pi * np.pi, (flicker*block_len))
-	t = np.zeros(x.shape)
-	s = ((np.cos(x)* -1)+1) * 0.5
-	s = (s * 0.96) + 0.02
-	z = np.zeros(x.shape)
-	target = []
-	draw_target = []
-	t[0:-1:step] = 1
-	for block in range(block_num):
-		target.extend(s)
-		draw_target.extend(t)
-
-	x = np.linspace(0,ntnum_pi * np.pi, (flicker*block_len)) 
-	s = ((np.cos(x)* -1)+1) * 0.5
-	s = (s * 0.96) + 0.02
-	z = np.zeros(x.shape)
-	nontarget = []
-	for block in range(block_num):
-		nontarget.extend(s)
-
-#establishes flicker rate
-sl = (1/args.fcps) #.0833, or ~12Hz
-timing = [sl * trial for trial in range(len(target))]
-
+###############################################################################
+#setup psychopy objects
 win = psychopy.visual.Window(
     size=[1024, 768],
     units="pix",
@@ -102,7 +110,12 @@ if check_tar != 'r':
 fixate = psychopy.visual.ShapeStim(
     win=win,
     units="pix",
-    vertices=((-3,.5),(-.5,.5),(-.5,3),(.5,3),(.5,.5),(3,.5),(3,-.5),(.5,-.5),(.5,-3),(-.5,-3),(-.5,-.5),(-3,-.5)),
+    vertices=((-3,.5),(-.5,.5),
+              (-.5,3),(.5,3),
+              (.5,.5),(3,.5),
+              (3,-.5),(.5,-.5),
+              (.5,-3),(-.5,-3),
+              (-.5,-.5),(-3,-.5)),
     size=5,
     fillColor=[-0.25, -0.25, -0.25],
     lineColor=[-1, -1, -1]
@@ -129,6 +142,8 @@ if check_tar != 'r':
 
 event.Mouse(visible=False)
 
+##
+#Setup clock for timing
 clock = core.Clock()
 if show_inst is True:
 	for txt in inst_text:
@@ -140,51 +155,137 @@ if show_inst is True:
 			pass
 	fixate.draw()
 	win.flip()
+    
+###############################################################################
+#Calculate Checkerboards
+
+#Cut time into repeated blocks to allow for even divisibility by different freqs
+block_len = (1/tcps) * (1/ntcps) * 10
+#length of total video in s
+seconds += (seconds % block_len)
+#length of video in blocks
+block_num = int(seconds / block_len)
+
+#used to calculate aount of cycles per block
+tnum_pi = float(tcps) * block_len
+ntnum_pi = float(ntcps) * block_len
+
+#Calculate times where fade is calculated
+SampleBase_target = np.linspace(0,tnum_pi * np.pi, (flicker*block_len))
+SampleBase_ntarget = np.linspace(0,ntnum_pi * np.pi, (flicker*block_len)) 
+
+#caclulate checkerboard fade
+# *Fade ranges from 0 to 1 in sin wave 
+targetFade_block = ((np.cos(SampleBase_target)* -1)+1) * 0.5
+targetFade_block  = (targetFade_block  * 0.96) + 0.02
+ntargetFade_block = ((np.cos(SampleBase_ntarget)* -1)+1) * 0.5
+ntargetFade_block = (ntargetFade_block * 0.96) + 0.02
 
 
-tlist = []
+#find when to draw the target in each block
+#the target is drawn 
+t = np.zeros(SampleBase_target.shape)
+step = int((flicker*block_len) / (tnum_pi / 2))
+t[0:-1:step] = 1
+
+#find fade values for the whole video by adding each block together
+targetFade = []
+ntargetFade = []
+draw_target = []
+for block in range(block_num):
+    targetFade.extend(targetFade_block)
+    ntargetFade.extend(ntargetFade_block)
+    draw_target.extend(t)
+
+#establishes flicker rate
+sl = (1/args.fcps) #.0833, or ~12Hz
+timing = [sl * trial for trial in range(len(targetFade))]
+
+
+
+##############################################################################
+#setup experiment
+
+#wait for user to press spacebar to start experiment
 waiting = True
 while waiting:
 	if event.getKeys(keyList=["space"]):
 		waiting = False
-
 clock.reset()
 
+#draw fixate point, will not change throughout experiment
 fixate.size = 4
 fixate.draw()
 win.flip()
 
+#wait half a second before starting 
 while clock.getTime() < 0.5:
 	pass
+clock.reset()
 
+#fixate point drawn again
 fixate.size = 3
 fixate.draw()
 win.flip()
 
+#set starting checkerboard contrast to 1
 con = 1
-for k in range(len(target)):
-	#timg.contrast = target[k] * ((k % 2) * -1)
-	timg.contrast = con
-	timg.setOpacity(target[k])
-	timg.draw()
-	ntimg.contrast = con
-	ntimg.setOpacity(nontarget[k])
-	ntimg.draw()
-	if draw_target[k] == 1:
-		target_obj.draw()
-	fixate.draw()
-	while clock.getTime() < timing[k]:
-		pass
-	win.flip()
-	tlist.append(clock.getTime())
-	timg.contrast *= -1
-	if event.getKeys(keyList=["escape"]):
-         core.quit()
-	con *= -1
-dur = np.ones(len(target)) * sl
 
+#create empty timing list
+tlist = []
+
+###############################################################################
+#run experiment
+for frame in range(len(targetFade)):
+    
+    #set alternating contrast on both checkerboards
+    timg.contrast = con
+    ntimg.contrast = con
+    con *= -1
+    
+    #set opacity to calculated fade
+    timg.setOpacity(targetFade[frame])
+    ntimg.setOpacity(ntargetFade[frame])
+    
+    #draw checkerboards
+    timg.draw()
+    ntimg.draw()
+    
+    #Draw target at given times, this should occur every time the fade finishes
+    #   an occilation
+    if draw_target[frame] == 1:
+		target_obj.draw()
+    
+    #redraw fixation point
+    fixate.draw()
+    
+    #ensure timing matches before writing to screen
+    while clock.getTime() < timing[frame]:
+        pass
+    
+    #write to screen and record time written
+    win.flip()
+    tlist.append(clock.getTime())
+    
+    #quit program and close window when esc is pressed
+    if event.getKeys(keyList=["escape"]):
+        win.close() 
+        core.quit()
+        
+win.close()
+
+
+###############################################################################
+#save timings in output file
+
+
+dur = np.ones(len(targetFade)) * sl # theoretical duration between frames
+
+#convert target fade rate to file-readable format
 cps_str = str(args.tcps).replace('.','_')
-outname = "{}_{}_{}.ons".format(args.subid, args.type,cps_str)
+
+#get output file name as <subjectid>_<target_rate>
+outname = "{}_{}.ons".format(args.subid, cps_str)
 
 while path.exists(outname) is True:
 	base = outname.split('.')[0]
@@ -192,7 +293,7 @@ while path.exists(outname) is True:
 
 f = open(outname,'w')
 for k in range(len(timing)):
-    f.write('{:1.3f} {:1.3f} {:1.3f}\n'.format(timing[k], dur[k], target[k]))
+    f.write('{:1.3f} {:1.3f} {:1.3f}\n'.format(timing[k], dur[k], targetFade[k]))
 f.close()
 
 pickle.dump(tlist,open('test.p','w'))
