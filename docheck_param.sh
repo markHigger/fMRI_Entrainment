d530babaf61d0c563487eb6b66c171235e7b3dc3
Jessica Cloud <jcloud@admins-MacBook-Pro.local>
2018-08-31 10:35:59 -0400

    edited bash to include eyetracking

--- docheck_param.sh
+++ docheck_param.sh
@@ -6,7 +6,10 @@ source activate psypy
 #change to appropriate directory
 cd /Users/jcloud/fmri_entrainment
 
-#run programs
+#run eyetracking calibration
+python eyetracking_initiation_calibration.py -subid '000'
+
+#run study
 python docheck_stim_12hz_split.py -subid ‘000’ -tcpsec .1 -ntcpsec .1 -seconds 180
 python docheck_stim_12hz_split.py -subid ‘000’ -tcpsec .2 -ntcpsec .2 -seconds 180 -tside ‘l’
 python docheck_stim_12hz_split.py -subid ‘000’ -tcpsec .5 -ntcpsec .5 -seconds 180
